1e1ab9e9b1d5ecd16dc1e66ef2a57176b33711b1
joao-manoel <joaomanoel.ms.br@gmail.com>
2023-07-13 08:20:12 -0400

    ref: refactory code

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from 'next/server'
 export default function middleware(request: NextRequest) {
   const token = request.cookies.get('rscore.token')?.value
 
+  // const pathname = request.nextUrl.pathname
+
   const signInURL = new URL('/', request.url)
 
   if (!token) {
